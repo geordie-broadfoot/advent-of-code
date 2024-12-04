64ae833e57b8d2a0fe4e822fb949e1a75fef7629
geordie-broadfoot <149107205+geordie-broadfoot@users.noreply.github.com>
2024-12-03 22:11:49 -0600

    day 3 solution

--- 2024/day3/index.js
+++ 2024/day3/index.js
@@ -8,15 +8,37 @@ const parseInput = (input) => {
 puzzle.setPart1((rawInput) => {
   const input = parseInput(rawInput)
 
-  let output = 0
+  const pattern = /mul\(\d+?,\d+?\)/g
 
-  return output
+  return input.match(pattern).reduce((acc, line) => {
+    const nums = line.split("").slice(4, -1).join("").split(",").map(Number)
+    return acc + nums[0] * nums[1]
+  }, 0)
 })
 
 puzzle.setPart2((rawInput) => {
   const input = parseInput(rawInput)
 
+  const pattern = /(mul\(\d+?,\d+?\)|do\(\)|don't\(\))/g
+
   let output = 0
+  let on = true
+  const matches = input.match(pattern)
+  console.log(matches)
+  for (let i = 0; i < matches.length; i++) {
+    const m = matches[i]
+    if (m.match("don't()")) {
+      console.log("disabling")
+      on = false
+    } else if (m.match("do()")) {
+      on = true
+      console.log("enabling")
+    } else if (on) {
+      const nums = m.split("").slice(4, -1).join("").split(",").map(Number)
+      console.log("Multiplying", nums)
+      output += nums[0] * nums[1]
+    }
+  }
 
   return output
 })
