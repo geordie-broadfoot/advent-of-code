6e4f867a69e88c9556e50bd66061a112ee75d677
geordie-broadfoot <149107205+geordie-broadfoot@users.noreply.github.com>
2024-12-07 10:15:06 -0600

    day7 solution

--- 2024/day7/index.js
+++ 2024/day7/index.js
@@ -1,24 +1,75 @@
 import { Puzzle } from "../../utils/puzzle.cjs"
-const puzzle = new Puzzle("Day 1, 2023")
+const puzzle = new Puzzle("Day 7, 2024")
 
 const parseInput = (input) => {
-  return input
+  return input.split("\n").map((row) => {
+    const [sum, rest] = row.split(": ")
+
+    const numbers = rest.split(" ").map(Number)
+
+    return {
+      output: Number(sum),
+      numbers,
+    }
+  })
+}
+
+const recursiveCheck = (set, operators, calc = "", depth = 0) => {
+  if (depth >= set.numbers.length) {
+    // used up all of the numbers
+    const out = eval(calc)
+    if (out == set.output) {
+      console.log("   --- found the combo: ", calc, eval(calc))
+      return true
+    }
+    return false
+  }
+
+  if (depth === 0) {
+    console.log("Calculating set ", set.numbers.join(" "))
+    return recursiveCheck(set, operators, calc + set.numbers[0], 1)
+  } else {
+    for (let i = 0; i < operators.length; i++) {
+      const op = operators[i]
+
+      let newCalc = calc
+
+      if (op === "|") {
+        newCalc += "" + set.numbers[depth]
+      } else {
+        newCalc += " " + op + " " + set.numbers[depth]
+      }
+
+      const valid = recursiveCheck(
+        set,
+        operators,
+        "" + eval(newCalc),
+        depth + 1
+      )
+
+      if (valid) return true
+    }
+  }
+
+  return false
 }
 
 puzzle.setPart1((rawInput) => {
   const input = parseInput(rawInput)
 
-  let output = 0
+  const valid = input.filter((set) => recursiveCheck(set, ["*", "+"]))
 
-  return output
+  console.log(valid)
+
+  return valid.reduce((a, r) => a + r.output, 0)
 })
 
 puzzle.setPart2((rawInput) => {
   const input = parseInput(rawInput)
 
-  let output = 0
+  const valid = input.filter((set) => recursiveCheck(set, ["*", "+", "|"]))
 
-  return output
+  return valid.reduce((a, r) => a + r.output, 0)
 })
 
 puzzle.run()
