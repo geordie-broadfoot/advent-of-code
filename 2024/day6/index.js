e9a4f158d150a3cf8c6a505b99e8695de0b7d5a2
geordiebroadfoot <geordie.broadfoot@gmail.com>
2025-12-01 12:31:33 -0600

    2025 template and day1 solution

--- 2024/day6/index.js
+++ 2024/day6/index.js
@@ -1,16 +1,113 @@
 import { Puzzle } from "../../utils/puzzle.cjs"
-const puzzle = new Puzzle("Day 1, 2023")
+const puzzle = new Puzzle("Day 6, 2024")
 
-const parseInput = (input) => {
-  return input
+const WALL = /#/g
+const OPEN = /[\.\^>V<]/g
+
+const DIRS = {
+  n: {
+    x: 0,
+    y: -1,
+    sym: "^",
+  },
+  e: {
+    x: 1,
+    y: 0,
+    sym: ">",
+  },
+  s: {
+    x: 0,
+    y: 1,
+    sym: "v",
+  },
+  w: {
+    x: -1,
+    y: 0,
+    sym: "<",
+  },
 }
 
-puzzle.setPart1((rawInput) => {
+const TURNS = {
+  n: "e",
+  e: "s",
+  s: "w",
+  w: "n",
+}
+
+const parseInput = (input) => {
+  const map = input.split("\n").map((row) => {
+    return row.split("")
+  })
+
+  const startPos = { x: -1, y: -1 }
+
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (getGridCell(map, x, y) === "^") {
+        startPos.x = x
+        startPos.y = y
+      }
+    }
+  }
+  return {
+    map,
+    startPos,
+  }
+}
+
+const getGridCell = (grid, x, y) => {
+  if (x < 0 || x > grid[0].length || y < 0 || y > grid.length - 1) return ""
+  return grid[y][x]
+}
+
+puzzle.setPart1((rawInput, testing) => {
   const input = parseInput(rawInput)
+  let isOnMap = true
 
-  let output = 0
+  let currentDir = "n"
+  const currentPos = { ...input.startPos }
+  let steps = 0
+  while (isOnMap) {
+    // Walk
+    steps++
+    const nextPos = {
+      x: currentPos.x + DIRS[currentDir].x,
+      y: currentPos.y + DIRS[currentDir].y,
+    }
+    const next = getGridCell(input.map, nextPos.x, nextPos.y)
 
-  return output
+
+    if (next === "") {
+      // outside of map
+      isOnMap = false
+    } else if (
+      ['.', '^','>','<','v'].includes(next)
+    ) {
+      // Step forward
+      currentPos.x = nextPos.x
+      currentPos.y = nextPos.y
+
+      // Put direction symbol onto map
+     
+      input.map[currentPos.y][currentPos.x] = next === '.' ? DIRS[currentDir].sym : 'X'
+    } else if (next === "#") {
+      // Hit wall, turn right
+      currentDir = TURNS[currentDir]
+      input.map[currentPos.y][currentPos.x] = DIRS[currentDir].sym
+    }
+  }
+
+  const visitedTiles = input.map.reduce(
+    (sum, r) =>
+      sum +
+      r.reduce((s, t) => {
+        if (t === "." || t === "#") return s
+        return s + 1
+      }, 0),
+    0
+  )
+testing && console.log(input.map.map(r => r.join('')).join('\n').replace(/\./g, ' '))
+  return visitedTiles
 })
 
 puzzle.setPart2((rawInput) => {
---
+++ 2025/day11/index.js
@@ -0,0 +1,24 @@
+import { Puzzle } from "../../utils/puzzle.cjs"
+const puzzle = new Puzzle("Day 1, 2023")
+
+const parseInput = (input) => {
+  return input
+}
+
+puzzle.setPart1((rawInput) => {
+  const input = parseInput(rawInput)
+
+  let output = 0
+
+  return output
+})
+
+puzzle.setPart2((rawInput) => {
+  const input = parseInput(rawInput)
+
+  let output = 0
+
+  return output
+})
+
+puzzle.run()
